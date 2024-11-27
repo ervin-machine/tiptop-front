8f289dcf81405fa2ec15e7eb64882d3b42c36073
Ervin Hodzic <hodzicervin462001@gmail.com>
2024-11-27 21:33:23 +0100

    API

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const API_URL =
   process.env.REACT_APP_API_URL || // Use Vercel's environment variable
   (process.env.NODE_ENV === "production"
-    ? "https://tiptop-back.onrender.com" // Fallback for production
+    ? "https://tiptop-back.onrender.com/api" // Fallback for production
     : "http://localhost:5000"); // Fallback for development
 
 export default API_URL;
