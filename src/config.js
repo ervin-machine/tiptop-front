6d21948b3f1cacbe3a3b62fcfa8e6ceb3123053d
Ervin Hodzic <hodzicervin462001@gmail.com>
2024-12-12 17:56:31 +0100

    Update API url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_URL =
-  process.env.REACT_APP_API_URL || // Use Vercel's environment variable
+  "tiptop-recruit-125-gqefaxguh6a2fddw.centralus-01.azurewebsites.net" || // Use Vercel's environment variable
   (process.env.NODE_ENV === "production"
     ? "tiptop-recruit-125-gqefaxguh6a2fddw.centralus-01.azurewebsites.net" // Fallback for production
     : "http://localhost:5000/api"); // Fallback for development
